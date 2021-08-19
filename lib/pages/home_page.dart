6f000977b0a4267e9946e89c4aaa381975107479
marcelo tito copa <mat@matPc.local>
2021-08-19 00:14:02 -0400

    habre la camara y muestra por consola lo leido- solo android

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,24 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:qr_reader/pages/direcciones_page.dart';
+import 'package:qr_reader/providers/ui_provider.dart';
+
 import 'package:qr_reader/widgets/custom_navigatorbar.dart';
 import 'package:qr_reader/widgets/scan_button.dart';
 
-class HomePage extends StatelessWidget {
-  
+import 'mapas_pages.dart';
 
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:AppBar(
-        elevation:0,
+      appBar: AppBar(
+        elevation: 0,
         title: Text('Historial'),
         actions: [
-          IconButton(onPressed: ()=>{}, icon: Icon(Icons.delete_forever))
+          IconButton(onPressed: () => {}, icon: Icon(Icons.delete_forever))
         ],
       ),
-      body: Center(child: Text('Home Page'),),
+      body: _HomePageBody(),
       bottomNavigationBar: CustomNavigationBar(),
-      floatingActionButton:ScanButton() ,
-      floatingActionButtonLocation:FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: ScanButton(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
+  }
+}
+
+class _HomePageBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+//obtener el selected menu opt
+final uiProvider= Provider.of<UiProvider>(context);
+
+//cambiar para mostrar la pagina respectiva
+
+    final currentIndex = uiProvider.selectedMenuOpt;
+    switch (currentIndex) {
+      case 0:
+        return MapasPage();
+      case 1:
+        return DireccionesPage();
+      default:
+        return MapasPage();
+    }
   }
 }
--- lib/widgets/scan_button.dart
+++ lib/widgets/scan_button.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
 class ScanButton extends StatelessWidget {
-  
-
   @override
   Widget build(BuildContext context) {
     return FloatingActionButton(
       elevation: 0,
-      child:Icon(Icons.qr_code_2_rounded),
-      onPressed: ()=>{},
+      child: Icon(Icons.qr_code_2_rounded),
+      onPressed: () async {
+        String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+            '#3D8BEF', ' cancelar ', false, ScanMode.QR);
+        print(barcodeScanRes);
+      },
     );
   }
 }
